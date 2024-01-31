c9a2576c7de7aafeaf6265332718480838ddd401
Wonjeong Park <75118947+ParkWonjeong@users.noreply.github.com>
2024-01-31 10:58:40 +0900

    Title: 그래프 탐색, Time: 169ms, Memory: 26MB, Status: Passed - CodeTree

--- 240131/그래프 탐색/graph-traversal.py
+++ 240131/그래프 탐색/graph-traversal.py
@@ -1,9 +1,27 @@
 N, M = tuple(map(int, input().split()))
-arr = [list(map(int, input().split())) for _ in range(M)]
-cnt = 0
 
-for elem in arr:
-    if 1 in elem:
-        cnt += 1
+#index를 1번 부터 사용하기 위해 m+1만큼 할당합니다.
+graph = [[] for _ in range(N + 1)]
 
-print(cnt)
+visited = [False for _ in range(N + 1)]
+vertex_cnt = 0
+
+
+def dfs(vertex):
+    global vertex_cnt
+    for curr_v in graph[vertex]:
+        if not visited[curr_v]:
+            visited[curr_v] = True
+            vertex_cnt += 1
+            dfs(curr_v)
+    
+    
+for i in range(M):
+    v1, v2 = tuple(map(int, input().split()))
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+
+visited[1] = True
+dfs(1)
+
+print(vertex_cnt)
