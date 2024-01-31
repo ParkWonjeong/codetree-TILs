57df859864d014d97552807cd45f9d0fb5ee0c97
Wonjeong Park <75118947+ParkWonjeong@users.noreply.github.com>
2024-01-31 11:13:57 +0900

    Title: 그래프 탐색, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240131/그래프 탐색/graph-traversal.py
+++ 240131/그래프 탐색/graph-traversal.py
@@ -1,25 +1,31 @@
 N, M = tuple(map(int, input().split()))
-graph = [[] for _ in range(N + 1)]
-
+graph = [
+    []
+    for _ in range(N + 1)
+]
 visited = [False for _ in range(N + 1)]
 vertex_cnt = 0
 
-
+# dfs 함수 정의
 def dfs(vertex):
+    # 전역 변수로 호출
     global vertex_cnt
+
+    # 재귀적으로 탐색 및 카운트
     for curr_v in graph[vertex]:
-        if not visited[curr_v]:
-            visited[curr_v] = True
+        if visited[curr_v] == 0:
+            visited[curr_v] = 1
             vertex_cnt += 1
             dfs(curr_v)
-    
-    
+
+# 입력 받은 후 인접 행렬에 추가
 for i in range(M):
     v1, v2 = tuple(map(int, input().split()))
+
     graph[v1].append(v2)
     graph[v2].append(v1)
 
-visited[1] = True
+visited[i] = 1
 dfs(1)
 
 print(vertex_cnt)
