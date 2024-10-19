4da3a9adebaabb825249f2ab47c54fd2243414b9
Wonjeong (Joseph) Park <75118947+ParkWonjeong@users.noreply.github.com>
2024-10-19 23:54:07 +0900

    Title: 나누고 빼면서 합하기, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 241019/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
+++ 241019/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -1,17 +1,20 @@
+# 변수 선언 및 입력
 n, m = tuple(map(int, input().split()))
+arr = [0] + list(map(int, input().split()))
 
-arr = list(map(int, input().split()))
+# 문제에서 구하고자 하는 값을 반환합니다.
+def get_answer():
+    global m
 
-def div_sub_func(m):
-    answer = 0
-    while m != 1:
+    return_value = 0
+    while m:
+        return_value += arr[m]
+
         if m % 2 == 0:
-            answer += arr[m - 1]
             m //= 2
         else:
-            answer += arr[m - 1]
             m -= 1
-    answer += arr[0]
-    return answer
 
-print(div_sub_func(m))
+    return return_value
+
+print(get_answer())
